50843f8cd5091facb55c43b3d7c289c5ecf24027
スケゾー <sukezo@Mac-mini.local>
2026-08-12 14:53:37 +0900

    Add ice age proposal page

--- src/worker.js
+++ src/worker.js
@@ -1,5 +1,12 @@
 export default {
   fetch(request, env) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/proposal/iceage") {
+      url.pathname = "/proposal/iceage/index.html";
+      return env.ASSETS.fetch(new Request(url, request));
+    }
+
     return env.ASSETS.fetch(request);
   }
 };
